5d33245733dc427ed2aad9d540eb9802f9430357
Marius Bernahrndt <bernahrndt@web.de>
2023-04-29 17:50:08 +0200

    Add preprocess and scatterplott and timeline

--- public/index.js
+++ public/index.js
@@ -1,191 +1,277 @@
-function clearChart() {
-	d3.select("#chart svg").remove();
-  }
-
-  function drawLine(ctx, x1, y1, x2, y2, color) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.strokeStyle = color;
-    ctx.stroke();
-  }
-
-  const data = [{"year":2017,"minage":14,"avg_playtime":90.0,"avg_rating":8.67527},{"year":2018,"minage":14,"avg_playtime":90.0,"avg_rating":8.65313},
-  {"year":2015,"minage":13,"avg_playtime":60.0,"avg_rating":8.56688},{"year":2021,"minage":14,"avg_playtime":120.0,"avg_rating":8.56684},
-  {"year":2017,"minage":14,"avg_playtime":106.375,"avg_rating":8.6449},{"year":2016,"minage":12,"avg_playtime":120.0,"avg_rating":8.39103},
-  {"year":2020,"minage":14,"avg_playtime":75.0,"avg_rating":8.55175},{"year":2016,"minage":14,"avg_playtime":210.0,"avg_rating":8.41817},
-  {"year":2011,"minage":13,"avg_playtime":165.0,"avg_rating":8.52576},{"year":2017,"minage":13,"avg_playtime":105.0,"avg_rating":8.36924},
-  {"year":2017,"minage":12,"avg_playtime":105.0,"avg_rating":8.42974},{"year":2020,"minage":14,"avg_playtime":90.0,"avg_rating":8.36497},
-  {"year":2015,"minage":14,"avg_playtime":120.0,"avg_rating":8.3355},{"year":2005,"minage":13,"avg_playtime":150.0,"avg_rating":8.26264},
-  {"year":2016,"minage":12,"avg_playtime":112.5,"avg_rating":8.25687},{"year":2016,"minage":14,"avg_playtime":102.5,"avg_rating":8.1865},
-  {"year":2011,"minage":12,"avg_playtime":60.0,"avg_rating":8.12806},{"year":2018,"minage":12,"avg_playtime":135.0,"avg_rating":8.33096},
-  {"year":2015,"minage":10,"avg_playtime":30.0,"avg_rating":8.0984},{"year":2007,"minage":14,"avg_playtime":90.0,"avg_rating":8.18934},
-  {"year":2013,"minage":13,"avg_playtime":100.0,"avg_rating":8.11031},{"year":2016,"minage":12,"avg_playtime":75.0,"avg_rating":8.18794},
-  {"year":2020,"minage":14,"avg_playtime":130.0,"avg_rating":8.55169},{"year":2012,"minage":12,"avg_playtime":105.0,"avg_rating":8.09724},
-  {"year":2019,"minage":10,"avg_playtime":55.0,"avg_rating":8.08126},{"year":2019,"minage":13,"avg_playtime":105.0,"avg_rating":8.61029},
-  {"year":2016,"minage":14,"avg_playtime":90.0,"avg_rating":8.14743},{"year":2018,"minage":10,"avg_playtime":75.0,"avg_rating":8.09206},
-  {"year":2020,"minage":12,"avg_playtime":75.0,"avg_rating":8.09201},{"year":2014,"minage":12,"avg_playtime":90.0,"avg_rating":8.06819},
-  {"year":2018,"minage":13,"avg_playtime":60.0,"avg_rating":8.07132},{"year":2015,"minage":13,"avg_playtime":67.5,"avg_rating":8.02556},
-  {"year":2011,"minage":14,"avg_playtime":150.0,"avg_rating":8.09071},{"year":2019,"minage":14,"avg_playtime":90.0,"avg_rating":8.20379},
-  {"year":2015,"minage":14,"avg_playtime":180.0,"avg_rating":8.09416},{"year":2019,"minage":14,"avg_playtime":67.5,"avg_rating":8.16512},
-  {"year":2017,"minage":12,"avg_playtime":90.0,"avg_rating":8.38036},{"year":2021,"minage":10,"avg_playtime":20.0,"avg_rating":8.29454},
-  {"year":2002,"minage":12,"avg_playtime":120.0,"avg_rating":7.94204},{"year":2013,"minage":12,"avg_playtime":120.0,"avg_rating":7.97952}];
-
-  console.log(data)
-
-
-  function Task_1(data) {
-    clearChart(); // Remove any previous chart elements
-  
-    console.log(data); // Log data to the console for debugging purposes
-  
-    // Set the dimensions and margins for the chart
-    var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-  
-    // Create an SVG container and group (g) element, and apply a transformation for the margins
-    var svg = d3.select("#chart")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
-    // Define the x-axis scale based on average playtime
-    const xScale = d3.scaleLinear()
-      .domain(d3.extent(data, d => d.avg_playtime))
-      .range([0, width]);
-  
-    // Define the y-axis scale based on average rating
-    const yScale = d3.scaleLinear()
-      .domain(d3.extent(data, d => d.avg_rating))
-      .range([height, 0]);
-  
-    // Create and position circle elements for each data point
-    svg.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('cx', d => xScale(d.avg_playtime))
-      .attr('cy', d => yScale(d.avg_rating))
-      .attr('r', 4)
-      .attr('fill', 'steelblue');
-  
-    // Create and position the x-axis
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-  
-    // Add the x-axis and label to the chart
-    svg.append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(xAxis)
-      .append('text')
-      .attr('x', width / 2)
-      .attr('y', margin.bottom )
-      .attr('fill', 'black')
-      .text('Average Playtime');
-  
-    // Add the y-axis and label to the chart
-    svg.append('g')
-      .call(yAxis)
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', -margin.left / 1.5)
-      .attr('x', -height / 2)
-      .attr('fill', 'black')
-      .text('Average Rating');
-
-  // Calculate the coefficients for the regression line
-  const regressionResult = ss.linearRegression(data.map(d => [d.avg_playtime, d.avg_rating]));
-
-  // Create a function to calculate the y-coordinate of the regression line for any given x-coordinate
-  const regressionLine = x => (regressionResult.m * x) + regressionResult.b;
-
-  // Create an array of x-coordinates for the regression line
-  const xCoords = d3.range(
-    d3.min(data, d => d.avg_playtime),
-    d3.max(data, d => d.avg_playtime),
-    (d3.max(data, d => d.avg_playtime) - d3.min(data, d => d.avg_playtime)) / 1000
-  );
-
-  // Calculate the corresponding y-coordinates
-  const lineData = xCoords.map(x => ({ x: x, y: regressionLine(x) }));
-
-  // Define a line generator
-  const lineGenerator = d3.line()
-    .x(d => xScale(d.x))
-    .y(d => yScale(d.y));
-
-  // Draw the regression line on the scatterplot
-  svg.append("path")
-    .datum(lineData)
-    .attr("fill", "none")
-    .attr("stroke", "red")
-    .attr("stroke-width", 1.5)
-    .attr("stroke-dasharray", "5,5")
-    .attr("d", lineGenerator);
-
-  }
 // this is working because of the import in the html file
 // https://socket.io/docs/v4/client-installation/#standalone-build 
 const socket = io()
+var chartType = "unused"
+
 socket.on("connect", () => { 
   console.log("Connected to the webserver.")
 })
 socket.on("disconnect", () => {
   console.log("Disconnected from the webserver.") 
 })
-socket.on("example_data", (obj) => {
-   console.log(obj)
-})
-function Task_2() { 
-	clearChart();
-    // Set the data for the chart
-var data = [4, 8, 15, 16, 23, 42];
+socket.on("boardgames_data", (obj) => {
+  console.log("Received data:", obj)
 
-// Set the dimensions of the canvas
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
-	width = 960 - margin.left - margin.right,
-	height = 500 - margin.top - margin.bottom;
+  const preprocessedData = preprocessData(obj);
+  console.log("preprocessedData:", preprocessedData)
+  if (chartType === "scatterplot"){
+    draw_scatterplot_and_regressionline(preprocessedData);
+  } else if (chartType === "timeline"){
+    draw_year_minage_timeline(preprocessedData);
+  } else {
+    console.error("Unknown chart type received: ", chartType);
+  }
 
-// Set the ranges of the axes
-var x = d3.scaleBand()
-	.range([0, width])
-	.padding(0.1);
-var y = d3.scaleLinear()
-	.range([height, 0]);
+});
 
-// Create the SVG canvas
-var svg = d3.select("#chart")
-	.append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function clearChart() {
+  d3.select("#chart svg").remove();
+}
 
-// Scale the range of the data
-x.domain(data.map(function(d) { return d; }));
-y.domain([0, d3.max(data, function(d) { return d; })]);
+/////////////////////////////////////////// preprocess the data ////////////////////////////////////////////
 
-// Add the bars to the chart
-svg.selectAll(".bar")
-	.data(data)
-	.enter().append("rect")
-	.attr("class", "bar")
-	.attr("x", function(d) { return x(d); })
-	.attr("width", x.bandwidth())
-	.attr("y", function(d) { return y(d); })
-	.attr("height", function(d) { return height - y(d); });
+function preprocessData(data) {
+  console.log("Data in preprocessData:", data);
+  // Check for missing values
+  const missingValues = data.some((d) =>
+    Object.values(d).some((value) => value === null || value === undefined)
+  );
+  if (missingValues) throw new Error("Data contains missing values");
 
-// Add the x-axis to the chart
-svg.append("g")
-	.attr("transform", "translate(0," + height + ")")
-	.call(d3.axisBottom(x));
+  // Select variables to include in scatterplot matrix
+  const cols = [
+    "year",
+    "minplayers",
+    "maxplayers",
+    "minplaytime",
+    "maxplaytime",
+    "minage",
+    "rating",
+  ];
 
-// Add the y-axis to the chart
-svg.append("g")
-	.call(d3.axisLeft(y));
+  // Create a new array of objects with selected variables
+  const df_scatter = data.map((d) =>
+    cols.reduce(
+      (acc, col) => ({
+        ...acc,
+        [col]: d[col],
+      }),
+      {}
+    )
+  );
 
+  // Combine minplaytime and maxplaytime into a single average playtime variable
+  const avg_playtime = df_scatter.map((d) =>
+    (d.minplaytime + d.maxplaytime) / 2
+  );
+  const mean_avg_playtime = avg_playtime.reduce((acc, d) => acc + d, 0) / avg_playtime.length;
+  df_scatter.forEach((d) => {
+    d.avg_playtime = (d.minplaytime + d.maxplaytime) / 2 > 300 ? mean_avg_playtime : (d.minplaytime + d.maxplaytime) / 2;
+  });
+
+  // Extract the 'rating' column into a separate Series
+  const pd_avg_rating = df_scatter.map((d) => d.rating.rating);
+
+  // Add the pd_avg_rating array as a new property to the df_scatter array
+  df_scatter.forEach((d, i) => {
+    d.avg_rating = pd_avg_rating[i];
+  });
+
+  // Drop the original minplaytime and maxplaytime properties
+  df_scatter.forEach((d) => {
+    delete d.minplaytime;
+    delete d.maxplaytime;
+    delete d.rating;
+    delete d.minplayers;
+    delete d.maxplayers;
+  });
+
+  // Return the preprocessed data as an array of objects
+  return df_scatter;
+};
+
+////////////////////////////////////////////////// Task_1 //////////////////////////////////////////////////
+
+function Task_1() { 
+  chartType = "scatterplot"
+  socket.emit("get_boardgames_data")
+}
+
+function draw_scatterplot_and_regressionline(data) {
+  clearChart();
+
+  // Set the dimensions and margins for the chart
+  var margin = {top: 50, right: 30, bottom: 30, left: 50},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+  // Create an SVG container and group (g) element, and apply a transformation for the margins
+  var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add a title to the chart
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", -margin.top / 2)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "21px")
+    .attr("font-weight", "bold")
+    .text("Correlation: Rating & Playtime");
   
+  // Define the x-axis scale based on average playtime
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.avg_playtime))
+    .range([0, width]);
+
+  // Define the y-axis scale based on average rating
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.avg_rating))
+    .range([height, 0]);
+
+  // Create and position circle elements for each data point
+  svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d.avg_playtime))
+    .attr('cy', d => yScale(d.avg_rating))
+    .attr('r', 4)
+    .attr('fill', 'steelblue');
+
+  // Create and position the x-axis
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  // Add the x-axis and label to the chart
+  svg.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(xAxis)
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', margin.bottom + 10)
+    .attr('fill', 'black')
+    .attr("font-size", "16px")
+    .text('Average Playtime in minutes');
+
+  // Add the y-axis and label to the chart
+  svg.append('g')
+    .call(yAxis)
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', -margin.left / 1.5 )
+    .attr('x', -height / 2)
+    .attr('fill', 'black')
+    .attr("font-size", "16px")
+    .text('Rating');
+
+// Calculate the coefficients for the regression line
+const regressionResult = ss.linearRegression(data.map(d => [d.avg_playtime, d.avg_rating]));
+
+// Create a function to calculate the y-coordinate of the regression line for any given x-coordinate
+const regressionLine = x => (regressionResult.m * x) + regressionResult.b;
+
+// Create an array of x-coordinates for the regression line
+const xCoords = d3.range(
+  d3.min(data, d => d.avg_playtime),
+  d3.max(data, d => d.avg_playtime),
+  (d3.max(data, d => d.avg_playtime) - d3.min(data, d => d.avg_playtime)) / 200
+);
+
+// Calculate the corresponding y-coordinates
+const lineData = xCoords.map(x => ({ x: x, y: regressionLine(x) }));
+
+// Define a line generator
+const lineGenerator = d3.line()
+  .x(d => xScale(d.x))
+  .y(d => yScale(d.y));
+
+// Draw the regression line on the scatterplot
+svg.append("path")
+  .datum(lineData)
+  .attr("fill", "none")
+  .attr("stroke", "red")
+  .attr("stroke-width", 1.5)
+  .attr("stroke-dasharray", "5,5")
+  .attr("d", lineGenerator);
+
+}
+////////////////////////////////////////////////// Task_2 //////////////////////////////////////////////////
+
+function Task_2() { 
+  chartType = "timeline"
+  socket.emit("get_boardgames_data")
+}
+
+function draw_year_minage_timeline(data) {
+  clearChart();
+
+  // Set the dimensions and margins for the chart
+  var margin = {top: 50, right: 30, bottom: 30, left: 50},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+  // Create an SVG container and group (g) element, and apply a transformation for the margins
+  var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Define the x-axis scale based on the year
+  const xScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d.year), d3.max(data, d => d.year)])
+    .range([0, width]);
+
+  // Define the y-axis scale based on the minage
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.minage)])
+    .range([height, 0]);
+
+  // Create and position the x-axis
+  const xAxis = d3.axisBottom(xScale).tickFormat(d3.format(".0f"));
+
+  // Add the x-axis and label to the chart
+  svg.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(xAxis)
+    .append('text')
+    .attr('y', 30)
+    .attr('x', width / 2)
+    .attr('fill', 'black')
+    .attr("font-size", "16px")
+    .text('Year');
+
+  // Create and position the y-axis
+  const yAxis = d3.axisLeft(yScale);
+
+  // Add the y-axis and label to the chart
+  svg.append('g')
+    .call(yAxis)
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', -margin.left / 1.5)
+    .attr('x', -height / 2)
+    .attr('fill', 'black')
+    .attr("font-size", "16px")
+    .text('Minimum Age');
+
+  // Create a line generator function
+  const lineGenerator = d3.line()
+    .x(d => xScale(d.year))
+    .y(d => yScale(d.minage));
+
+  // Sort the data by year to ensure the line connects points in the correct order
+  data.sort((a, b) => a.year - b.year);
+
+  // Create and position the line path
+  svg.append('path')
+    .attr('d', lineGenerator(data))
+    .attr('fill', 'none')
+    .attr('stroke', 'steelblue')
+    .attr('stroke-width', 2);
 }
--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-const fs = require("fs")
-const port = 6789
-let message = `Webserver is running on port ${port}.`
+const fs = require("fs");
+const path = require("path");
+const port = 6789;
+let message = `Webserver is running on port ${port}.`;
 
-const express = require("express") 
-const app = express() 
-app.use(express.static("public")) 
-const server = app.listen(port)
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const server = app.listen(port);
 
-console.log(message)
+console.log(message);
 
-const socket = require("socket.io") 
-const io = socket(server)
+const socket = require("socket.io");
+const io = socket(server);
 
-io.sockets.on("connection", (socket) => { 
-    console.log(`Client ${socket.id} connected.`)
+io.sockets.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected.`);
 
-let disconnect = () => {
-    console.log(`Client ${socket.id} disconnected.`)
-}
-let get_example_data = (parameters) => {
-    console.log(`Received data request with these parameters: ${parameters}`) 
-    fs.readFile("./data/testdata.json", "utf8", (err, data) => {
-    if (err) { 
-        console.error(err) 
-        return
+  let disconnect = () => {
+    console.log(`Client ${socket.id} disconnected.`);
+  };
+
+
+let get_boardgames_data = (parameters) => {
+  console.log(`Received data request with these parameters: ${parameters}`)
+  fs.readFile("./data/boardgames_40.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err)
+      return
     }
-    let json_data = JSON.parse(data) 
-    socket.emit("example_data", json_data)
-    }) 
+    let json_data = JSON.parse(data)
+    socket.emit("boardgames_data", json_data)
+  })
 }
 
-socket.on("disconnect", disconnect)
+  socket.on("disconnect", disconnect);
 
-socket.on("get_example_data", get_example_data)
-})
+  socket.on("get_boardgames_data", get_boardgames_data);
+});
