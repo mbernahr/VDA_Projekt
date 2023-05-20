46e138bb877b1703061d920389d83138a9e21902
Marius Bernahrndt <bernahrndt@web.de>
2023-05-20 18:48:11 +0200

    Add Task3

--- public/index.js
+++ public/index.js
@@ -18,6 +18,8 @@ socket.on("boardgames_data", (obj) => {
     draw_scatterplot_and_regressionline(preprocessedData);
   } else if (chartType === "timeline") {
     draw_year_minage_timeline(preprocessedData);
+  } else if (chartType === "lda") {
+    draw_lda(preprocessedData);
   } else {
     console.error("Unknown chart type received: ", chartType);
   }
@@ -41,26 +43,24 @@ var colorRegressionLine = "red";
 let colorScale = d3.scaleOrdinal(d3.schemeBlues[9]);
 
 function toggleColorblindMode() {
-    colorblindMode = !colorblindMode;
-    if (colorblindMode) {
-        // Change colors to be colorblind friendly
-        d3.selectAll("circle")
-            .attr("fill", function(d, i) {
-                // Use the color scale to assign colors
-                return colorScale(i);
-            })
-            .attr("stroke", "blue");
-        // Change regression line color
-        d3.selectAll(".regression-line")
-            .attr("stroke", "orange");
-    } else {
-        // Change colors back to the original colors
-        d3.selectAll("circle")
-            .attr("fill", colorCircleFill)
-            .attr("stroke", colorCircleStroke);
-        d3.selectAll(".regression-line")
-            .attr("stroke", colorRegressionLine);
-    }
+  colorblindMode = !colorblindMode;
+  if (colorblindMode) {
+    // Change colors to be colorblind friendly
+    d3.selectAll("circle")
+      .attr("fill", function (d, i) {
+        // Use the color scale to assign colors
+        return colorScale(i);
+      })
+    // Change regression line color
+    d3.selectAll(".regression-line")
+      .attr("stroke", "orange");
+  } else {
+    // Change colors back to the original colors
+    d3.selectAll("circle")
+      .attr("fill", colorCircleFill)
+    d3.selectAll(".regression-line")
+      .attr("stroke", colorRegressionLine);
+  }
 }
 
 /////////////////////////////////////////// preprocess the data ////////////////////////////////////////////
@@ -81,6 +81,8 @@ function preprocessData(data) {
     "maxplaytime",
     "minage",
     "rating",
+    "types",
+    "title",
   ];
 
   // Create a new array of objects with selected variables
@@ -103,6 +105,11 @@ function preprocessData(data) {
     d.avg_playtime = (d.minplaytime + d.maxplaytime) / 2 > 300 ? mean_avg_playtime : (d.minplaytime + d.maxplaytime) / 2;
   });
 
+  // Combine minplayer and maxplayer into a single average player variable
+  const avg_player = df_scatter.map((d) =>
+    (d.minplaytime + d.maxplaytime) / 2
+  );
+
   // Group and calculate the average "minage" by year and then add this as a new property to every game object in "df_scatter"
   const groupByYear = data.reduce((groups, item) => {
     const year = item.year;
@@ -112,12 +119,12 @@ function preprocessData(data) {
     groups[year].push(item.minage);
     return groups;
   }, {});
-  
+
   const avgMinageByYear = Object.keys(groupByYear).map(year => {
     const minages = groupByYear[year];
     const avgMinage = minages.reduce((total, minage) => total + minage, 0) / minages.length;
-    return {[year]: avgMinage};
-  }).reduce((acc, cur) => ({...acc, ...cur}), {});
+    return { [year]: avgMinage };
+  }).reduce((acc, cur) => ({ ...acc, ...cur }), {});
 
   df_scatter.forEach((d) => {
     d.avg_minage = avgMinageByYear[d.year];
@@ -139,16 +146,17 @@ function preprocessData(data) {
     d.num_of_reviews = pd_num_of_reviews[i];
   });
 
-  // Drop the original minplaytime and maxplaytime properties
+  // Drop the unused properties
   df_scatter.forEach((d) => {
     delete d.minplaytime;
     delete d.maxplaytime;
     delete d.rating;
-    delete d.minplayers;
-    delete d.maxplayers;
+    //delete d.minplayers;
+    //delete d.maxplayers;
   });
 
   // Return the preprocessed data as an array of objects
+  console.log("Preprocessed data:", df_scatter)
   return df_scatter;
 };
 
@@ -196,40 +204,41 @@ function draw_scatterplot_and_regressionline(data) {
     .range([height, 0]);
 
   // Define tooltip div
-  var div = d3.select("body").append("div")    
-  .attr("class", "tooltip")                
-  .style("opacity", 0);
+  var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
   // Create and position circle elements for each data point
   var circles = svg.selectAll('circle')
-  .data(data)
-  .enter()
-  .append('circle')
-  .attr('cx', d => xScale(d.avg_playtime))
-  .attr('cy', d => yScale(d.avg_rating))
-  .attr('r', 5)
-  .attr('fill', colorCircleFill)
-  .attr("stroke", colorCircleStroke)
-  .attr("stroke-width", 1.5)
-  .on('mouseover', function (event, d) {
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d.avg_playtime))
+    .attr('cy', d => yScale(d.avg_rating))
+    .attr('r', 5)
+    .attr('fill', colorCircleFill)
+    .attr("stroke", colorCircleStroke)
+    .attr("stroke-width", 1.5)
+    // Add hover effect
+    .on('mouseover', function (event, d) {
       d3.select(this).transition()
-          .duration('100')
-          .attr("r", 7);
+        .duration('100')
+        .attr("r", 7);
       div.transition()
-          .duration(100)
-          .style("opacity", 1);
-      div.html("Playtime: " + d3.format(".2f")(d.avg_playtime) + ", Rating: " + d3.format(".2f")(d.avg_rating))
-          .style("left", (event.pageX + 10) + "px")
-          .style("top", (event.pageY - 15) + "px");
-  })
-  .on('mouseout', function (event, d) {
+        .duration(100)
+        .style("opacity", 1);
+      div.html("Titel: " + d.title + "<br>Playtime: " + d3.format(".2f")(d.avg_playtime) + ", Rating: " + d3.format(".2f")(d.avg_rating))
+        .style("left", (event.pageX + 10) + "px")
+        .style("top", (event.pageY - 15) + "px");
+    })
+    .on('mouseout', function (event, d) {
       d3.select(this).transition()
-          .duration('200')
-          .attr("r", 5);
+        .duration('200')
+        .attr("r", 5);
       div.transition()
-          .duration('200')
-          .style("opacity", 0);
-  });
+        .duration('200')
+        .style("opacity", 0);
+    });
 
   // Create and position the x-axis
   const xAxis = d3.axisBottom(xScale);
@@ -381,38 +390,220 @@ function draw_year_minage_timeline(data) {
     .attr('stroke-width', 2);
 
   // Define tooltip div
-  var div = d3.select("body").append("div")    
-  .attr("class", "tooltip")                
-  .style("opacity", 0);
+  var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
   // Create and position circle elements for each data point
   var circles = svg.selectAll('circle')
-  .data(data)
-  .enter()
-  .append('circle')
-  .attr('cx', d => xScale(d.year))
-  .attr('cy', d => yScale(d.avg_minage))
-  .attr('r', 5)
-  .attr('fill', colorCircleFill)
-  .attr("stroke", colorCircleStroke)
-  .attr("stroke-width", 1.5)
-  .on('mouseover', function (event, d) {
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d.year))
+    .attr('cy', d => yScale(d.avg_minage))
+    .attr('r', 5)
+    .attr('fill', colorCircleFill)
+    .attr("stroke", colorCircleStroke)
+    .attr("stroke-width", 1.5)
+    // Add hover effect
+    .on('mouseover', function (event, d) {
       d3.select(this).transition()
-          .duration('100')
-          .attr("r", 7);
+        .duration('100')
+        .attr("r", 7);
       div.transition()
-          .duration(100)
-          .style("opacity", 1);
+        .duration(100)
+        .style("opacity", 1);
       div.html("Year: " + (d.year) + ", avg. min. Age: " + (d.avg_minage))
-          .style("left", (event.pageX + 10) + "px")
-          .style("top", (event.pageY - 15) + "px");
-  })
-  .on('mouseout', function (event, d) {
+        .style("left", (event.pageX + 10) + "px")
+        .style("top", (event.pageY - 15) + "px");
+    })
+    .on('mouseout', function (event, d) {
       d3.select(this).transition()
-          .duration('200')
-          .attr("r", 5);
+        .duration('200')
+        .attr("r", 5);
       div.transition()
-          .duration('200')
-          .style("opacity", 0);
-  });
+        .duration('200')
+        .style("opacity", 0);
+    });
+}
+
+////////////////////////////////////////////////// Task_3 //////////////////////////////////////////////////
+
+function Task_3() {
+  chartType = "lda"
+  socket.emit("get_boardgames_data")
+}
+
+function draw_lda(data) {
+  clearChart();
+  colorblindMode = false;
+
+
+  // Create a array with the most dominant game category from each game
+  var classCounts = {};
+  for (let game of data) {
+    for (let count of game.types.categories) {
+      if (!classCounts.hasOwnProperty(count.name)) {
+        classCounts[count.name] = 1;
+      } else {
+        classCounts[count.name]++;
+      }
+    }
+  }
+
+  let highestOrderedCategories = [];
+  for (let game of data) {
+    let tmp = [];
+    for (let count of game.types.categories) {
+      tmp.push(count.name)
+    }
+    tmp.sort((a, b) => classCounts[b] - classCounts[a]);
+    if (tmp.length > 0) {
+      highestOrderedCategories.push(tmp[0]);
+    }
+  }
+  console.log("highestOrderedCategories: ", highestOrderedCategories);
+
+  // Create a array with only the numerical data from each game
+  var numberData = []
+  const title = []
+
+  for (let game of data) {
+    var tmp = [];
+    tmp.push(game.avg_rating)
+    tmp.push(game.num_of_reviews)
+    tmp.push(game.avg_playtime)
+    tmp.push(game.minage)
+    tmp.push(game.minplayers)
+    tmp.push(game.maxplayers)
+    //tmp.push(game.year)
+
+    title.push(game.title)
+    numberData.push(tmp)
+  }
+
+  console.log("numberData:", numberData)
+
+  // LDA wit 2 dimensions
+  const X = druid.Matrix.from(numberData);
+
+  const reductionLDA = new druid.LDA(X, { labels: highestOrderedCategories, d: 2 })
+  const result = reductionLDA.transform()
+
+  console.log(result.to2dArray)
+
+  // Set the dimensions and margins for the chart
+  var margin = { top: 50, right: 30, bottom: 30, left: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  // Create an SVG container and group (g) element, and apply a transformation for the margins
+  var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add a title to the chart
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", -margin.top / 2)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "21px")
+    .attr("font-weight", "bold")
+    .text("LDA dimensionality reduction ");
+
+  // Create color scale
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
+  console.log("colors: ", color)
+
+  // Create scales for the axes
+  var xScale = d3.scaleLinear()
+    .domain([d3.min(result, d => d[0]), d3.max(result, d => d[0])])
+    .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+    .domain([d3.min(result, d => d[1]), d3.max(result, d => d[1])])
+    .range([height, 0]);
+
+  // Create axes
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
+
+  // Append the axes to the SVG
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .call(yAxis);
+
+  // Create the scatterplot
+  var circles = svg.selectAll(".dot")
+    .data(result)
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("r", 3.5)
+    .attr("cx", function (d, i) { return xScale(d[0]); })
+    .attr("cy", function (d, i) { return yScale(d[1]); })
+    .style("fill", function (_, i) { return color(highestOrderedCategories[i]); });
+
+
+  // Define tooltip div
+  var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+  // Get unique categories for the legend
+  let uniqueCategories = Array.from(new Set(highestOrderedCategories));
+
+  // Legend
+  var legend = svg.selectAll(".legend")
+    .data(uniqueCategories)
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function (_, i) { return "translate(" + (width + 20) + "," + i * 20 + ")"; });
+
+  // Draw legend colored rectangles
+  legend.append("rect")
+    .attr("x", 0)
+    .attr("width", 24)
+    .attr("height", 24)
+    .style("fill", color);
+
+  // Draw legend text
+  legend.append("text")
+    .attr("x", 28)
+    .attr("y", 12)
+    .attr("dy", ".35em")
+    .style("text-anchor", "start")
+    .text(function (d) { return d; });
+
+  const results = result.to2dArray
+
+  // Add hover effect
+  circles
+    .on('mouseover', function (event, d) {
+      d3.select(this)
+        .attr("r", 7);
+      div.transition()
+        .duration(100)
+        .style("opacity", 1);
+
+      // Search for Index
+      let index = results.findIndex(item => item[0] === d[0] && item[1] === d[1]);
+
+      div.html("Titel: " + title[index] + "<br>Categorie: " + highestOrderedCategories[index])
+        .style("left", (event.pageX + 10) + "px")
+        .style("top", (event.pageY - 15) + "px");
+    })
+    .on('mouseout', function (event, d) {
+      d3.select(this)
+        .attr("r", 3.5);
+      div.transition()
+        .duration('200')
+        .style("opacity", 0);
+    });
+
 }
